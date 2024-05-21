f56f0870d3baceb8efb43b9c09959d5e84afbdc1
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-05-21 13:40:26 +0200

    Add Payroll

---
+++ Employees/PayRoll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    internal class PayRoll
+    {
+        private List<Employee> _employees;
+
+        public PayRoll()
+        {
+            _employees = new List<Employee>();
+        }
+
+        public void AddEmployee(string name, int salary)
+        {
+            _employees.Add(new Employee(name, salary)); 
+        }
+    }
+}
--- Employees/Program.cs
+++ Employees/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            PayRoll payRoll = new PayRoll();
+            payRoll.AddEmployee("Anna", 45000);
+
             Employee employee = new Employee("Kalle", 30000);
             employee.Name = "Nisse";
             int salary = employee.Salary;
