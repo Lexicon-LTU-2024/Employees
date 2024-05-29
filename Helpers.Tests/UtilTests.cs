5709bdd15eb714bafca2e70ecad753dea130dbe6
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-05-29 14:44:29 +0200

    WithMoq

--- Helpers.Tests/UtilTests.cs
+++ Helpers.Tests/UtilTests.cs
@@ -1,5 +1,6 @@
 using Employees.Helpers;
 using Employees.UI;
+using Moq;
 
 namespace Helpers.Tests
 {
@@ -21,6 +22,22 @@ namespace Helpers.Tests
 
             //Act
             var actual = Util.AskForString("", mockUI);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        } 
+        
+        [Fact]
+        public void AskForString_ShouldReturnExpectedString_WithMoq()
+        {
+            //Arrange
+            const string expected = "some value";
+            var mockUI = new Mock<IUI>();
+            mockUI.Setup(u => u.GetInput()).Returns(expected);
+
+            //Act
+            var actual = Util.AskForString("", mockUI.Object);
 
             //Assert
             Assert.Equal(expected, actual);
